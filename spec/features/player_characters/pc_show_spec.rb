7013e5ad19a275c10264961deacdbd8e847d8457
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-11 16:50:39 -0600

    Feat: add test for user story 4 in child player_character spec

--- spec/features/player_characters/pc_show_spec.rb
+++ spec/features/player_characters/pc_show_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'adventurer index', type: :feature do
-    it 'can present each adventurer with its attributes' do
+RSpec.describe 'adventurer show', type: :feature do
+    it 'can present a specific adventurer with its attributes' do
         # As a visitor
         # When I visit '/child_table_name'
         # Then I see each Child in the system including the Child's attributes:
         adventurer = PlayerCharacter.create!(player_name: 'Karl', character_name: 'Garbanzo', age: 47, character_level: 7, is_alive: false)
         adventurer_2 = PlayerCharacter.create!(player_name: 'Beebo', character_name: 'Onion', age: 12, character_level: 8, is_alive: true)
 
-        visit '/player_characters'
+        visit '/player_characters/:id'
         # save_and_open_page
         expect(page).to have_content(adventurer.player_name)
-        expect(page).to have_content(adventurer_2.player_name)
+        expect(page).to_not have_content(adventurer_2.player_name)
         expect(page).to have_content(adventurer.id)
         expect(page).to have_content(adventurer.character_name)
         expect(page).to have_content(adventurer.age)
