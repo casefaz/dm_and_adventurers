0f5df1413a74feadd1b6b727be8cbd19a951559d
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-12 16:52:58 -0600

    Add code and files to pass dungeon master player characters index  aka user story 5

--- spec/features/dungeon_masters/player_characters/index_spec.rb
+++ spec/features/dungeon_masters/player_characters/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'dungeon master and player_character index' do
     # When I visit '/parents/:parent_id/child_table_name'
     # Then I see each Child that is associated with that Parent with each Child's attributes:
     it 'can show each child with their attributes associated with the parent' do
-        lucius = DungeonMaster.create!(name: 'Ozy', number_of_players: 3, dm_active: 'true', level_range: 'high')
+        lucius = DungeonMaster.create!(name: 'Lucius', number_of_players: 3, dm_active: 'true', level_range: 'high')
         adventurer1 = lucius.player_characters.create!(player_name: 'Ed', character_name: 'Bonnet', character_level: 4,character_age: 32, character_class: 'Warlock', character_race: 'Tiefling', is_alive: 'true')
         adventurer2 = lucius.player_characters.create!(player_name: 'Stede', character_name: 'Teach', character_level: 5,character_age: 207, character_class: 'Swashbuckler', character_race: 'Genasi', is_alive: 'true')
         adventurer3 = lucius.player_characters.create!(player_name: 'Izzy', character_name: 'Hands', character_level: 3,character_age: 25, character_class: 'Bard', character_race: 'Half-Elf', is_alive: 'false')
@@ -15,7 +15,7 @@ RSpec.describe 'dungeon master and player_character index' do
         adventurer5 = frenchie.player_characters.create!(player_name: 'Oluwande', character_name: 'Jim', character_level: 3,character_age: 29, character_class: 'Rogue', character_race: 'Human', is_alive: 'true')
     
         visit "/dungeon_masters/#{lucius.id}/player_characters"
-    
+        save_and_open_page
         expect(page).to have_content(lucius.name)
         expect(page).to_not have_content(frenchie.name)
         expect(page).to_not have_content(adventurer4.name)
