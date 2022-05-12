a9b34b2854dea4073b24d46b1585646c2ca1d1a8
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-12 16:15:03 -0600

    Feat: add test for user story 5 dm and pc index

---
+++ spec/features/dungeon_masters/player_characters/index_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe 'dungeon master and player_character index' do
+    # As a visitor
+    # When I visit '/parents/:parent_id/child_table_name'
+    # Then I see each Child that is associated with that Parent with each Child's attributes:
+    it 'can show each child with their attributes associated with the parent' do
+        lucius = DungeonMaster.create!(name: 'Ozy', number_of_players: 3, dm_active: 'true', level_range: 'high')
+        adventurer1 = lucius.player_characters.create!(player_name: 'Ed', character_name: 'Bonnet', character_level: 4,character_age: 32, character_class: 'Warlock', character_race: 'Tiefling', is_alive: 'true')
+        adventurer2 = lucius.player_characters.create!(player_name: 'Stede', character_name: 'Teach', character_level: 5,character_age: 207, character_class: 'Swashbuckler', character_race: 'Genasi', is_alive: 'true')
+        adventurer3 = lucius.player_characters.create!(player_name: 'Izzy', character_name: 'Hands', character_level: 3,character_age: 25, character_class: 'Bard', character_race: 'Half-Elf', is_alive: 'false')
+    
+        frenchie = DungeonMaster.create!(name: 'Frenchie', number_of_players: 2, dm_active: 'true', level_range: 'mid')
+        adventurer4 = frenchie.player_characters.create!(player_name: 'Wee John', character_name: 'Gun Powder', character_level: 2,character_age: 50, character_class: 'Barbarian', character_race: 'Dwarf', is_alive: 'true')
+        adventurer5 = frenchie.player_characters.create!(player_name: 'Oluwande', character_name: 'Jim', character_level: 3,character_age: 29, character_class: 'Rogue', character_race: 'Human', is_alive: 'true')
+    
+        visit "/dungeon_masters/#{lucius.id}/player_characters"
+    
+        expect(page).to have_content(lucius.name)
+        expect(page).to_not have_content(frenchie.name)
+        expect(page).to_not have_content(adventurer4.name)
+        expect(page).to have_content(adventurer1.player_name)
+        expect(page).to have_content(adventurer2.player_name)
+        expect(page).to have_content(adventurer3.player_name)
+        expect(page).to have_content(adventurer1.character_name)
+        expect(page).to have_content(adventurer2.character_name)
+        expect(page).to have_content(adventurer3.character_name)
+        expect(page).to have_content(adventurer1.character_level)
+        expect(page).to have_content(adventurer1.character_age)
+        expect(page).to have_content(adventurer1.character_class)
+        expect(page).to have_content(adventurer1.character_race)
+        expect(page).to have_content(adventurer1.is_alive)
+        expect(page).to have_content(adventurer1.created_at)
+        expect(page).to have_content(advenurer1.updated_at)
+    end
+end
--- spec/features/player_characters/pc_index_spec.rb
+++ spec/features/player_characters/pc_index_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe 'adventurer index', type: :feature do
         # As a visitor
         # When I visit '/child_table_name'
         # Then I see each Child in the system including the Child's attributes:
-        jezebel = DungeonMaster.create!(name: 'Jezebel', number_of_players: 4, dm_active: true, level_range: 'mid')
-        adventurer = jezebel.player_characters.create!(player_name: 'karl', character_name: 'Onion', character_level: 7,character_age: 207, character_class: 'Bard', character_race: 'Dwarf', is_alive: true)
+        jezebel = DungeonMaster.create!(name: 'Jezebel', number_of_players: 4, dm_active: 'true', level_range: 'mid')
+        adventurer = jezebel.player_characters.create!(player_name: 'karl', character_name: 'Onion', character_level: 7,character_age: 207, character_class: 'Bard', character_race: 'Dwarf', is_alive: 'true')
 
-        adventurer_2 = jezebel.player_characters.create!(player_name: 'Beebo', character_name: 'Onion', character_level: 8,character_age: 39, character_class: 'Artificer', character_race: 'Halfling', is_alive: true)
+        adventurer_2 = jezebel.player_characters.create!(player_name: 'Beebo', character_name: 'Onion', character_level: 8,character_age: 39, character_class: 'Artificer', character_race: 'Halfling', is_alive: 'true')
 
         visit '/player_characters'
         # save_and_open_page
