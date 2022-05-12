4416e0cd568e796bd1d2d1776e3d89a817729463
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-12 15:06:56 -0600

    Fix: add additional information to erb files and tests after adding columns to player_character database

--- spec/features/player_characters/pc_show_spec.rb
+++ spec/features/player_characters/pc_show_spec.rb
@@ -5,17 +5,20 @@ RSpec.describe 'adventurer show', type: :feature do
         # As a visitor
         # When I visit '/child_table_name/:id'
         # Then I see the child with that id including the child's attributes:
-        adventurer = PlayerCharacter.create!(player_name: 'Karl', character_name: 'Garbanzo', age: 47, character_level: 7, is_alive: false)
-        adventurer_2 = PlayerCharacter.create!(player_name: 'Beebo', character_name: 'Onion', age: 12, character_level: 8, is_alive: true)
+        adventurer = PlayerCharacter.create!(player_name: 'karl', character_name: 'Falafel', character_level: 7,character_age: 207, character_class: 'Bard', character_race: 'Dwarf', is_alive: true)
+
+        adventurer_2 = PlayerCharacter.create!(player_name: 'Beebo', character_name: 'Onion', character_level: 8,character_age: 39, character_class: 'Artificer', character_race: 'Halfling', is_alive: true)
 
         visit "/player_characters/#{adventurer.id}"
-        save_and_open_page
+        # save_and_open_page
         expect(page).to have_content(adventurer.player_name)
         expect(page).to_not have_content(adventurer_2.player_name)
         expect(page).to have_content(adventurer.id)
         expect(page).to have_content(adventurer.character_name)
-        expect(page).to have_content(adventurer.age)
         expect(page).to have_content(adventurer.character_level)
+        expect(page).to have_content(adventurer.character_age)
+        expect(page).to have_content(adventurer.character_class)
+        expect(page).to have_content(adventurer.character_race)
         expect(page).to have_content(adventurer.is_alive)
         expect(page).to have_content(adventurer.created_at)
         expect(page).to have_content(adventurer.updated_at)
