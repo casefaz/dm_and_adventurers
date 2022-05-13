8aae117dfc8e020842d1b802d0917f087184d5da
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-13 16:58:39 -0600

    Update: tests to have more fun examples

--- spec/features/dungeon_masters/dm_index_spec.rb
+++ spec/features/dungeon_masters/dm_index_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe 'dm index page', type: :feature do
         # When I visit '/parents'
         # Then I see the name of each parent record in the system
         it 'can see the names of all the dms in the system' do
-            dm = DungeonMaster.create!(name: 'Ezra', number_of_players: 4, dm_active: true, level_range: 'mid')
-            dm_2 = DungeonMaster.create!(name: 'Mai', number_of_players: 7, dm_active: true, level_range: 'high')
-            dm_3 = DungeonMaster.create!(name: 'Kai', number_of_players: 5, dm_active: false, level_range: 'low')
-            dm_4 = DungeonMaster.create!(name: 'Lorelai', number_of_players: 9, dm_active: true, level_range: 'low')
+            dm = DungeonMaster.create!(name: 'Amethyst', number_of_players: 4, dm_active: true, level_range: 'mid')
+            dm_2 = DungeonMaster.create!(name: 'Marcelline', number_of_players: 7, dm_active: true, level_range: 'high')
+            dm_3 = DungeonMaster.create!(name: 'Lucius', number_of_players: 5, dm_active: false, level_range: 'low')
+            dm_4 = DungeonMaster.create!(name: 'Frenchie', number_of_players: 9, dm_active: true, level_range: 'low')
             
             visit '/dungeon_masters'
             # save_and_open_page
--- spec/features/player_characters/pc_index_spec.rb
+++ spec/features/player_characters/pc_index_spec.rb
@@ -1,27 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'adventurer index', type: :feature do
-    it 'can present each adventurer with its attributes' do
-        # As a visitor
-        # When I visit '/child_table_name'
-        # Then I see each Child in the system including the Child's attributes:
-        jezebel = DungeonMaster.create!(name: 'Jezebel', number_of_players: 4, dm_active: 'true', level_range: 'mid')
-        adventurer = jezebel.player_characters.create!(player_name: 'karl', character_name: 'Onion', character_level: 7,character_age: 207, character_class: 'Bard', character_race: 'Dwarf', is_alive: 'true')
+    describe 'user story 3' do 
+        it 'can present each adventurer with its attributes' do
+            # As a visitor
+            # When I visit '/child_table_name'
+            # Then I see each Child in the system including the Child's attributes:
+            marcelline = DungeonMaster.create!(name: 'Marcelline', number_of_players: 4, dm_active: 'true', level_range: 'mid')
+            adventurer = marcelline.player_characters.create!(player_name: 'Peppermint Butler', character_name: 'Onion', character_level: 7,character_age: 207, character_class: 'Wizard', character_race: 'Dwarf', is_alive: 'true')
 
-        adventurer_2 = jezebel.player_characters.create!(player_name: 'Beebo', character_name: 'Onion', character_level: 8,character_age: 39, character_class: 'Artificer', character_race: 'Halfling', is_alive: 'true')
+            adventurer_2 = marcelline.player_characters.create!(player_name: 'Beemo', character_name: 'Rainbow', character_level: 8,character_age: 39, character_class: 'Artificer', character_race: 'Halfling', is_alive: 'true')
 
-        visit '/player_characters'
-        # save_and_open_page
-        expect(page).to have_content(adventurer.player_name)
-        expect(page).to have_content(adventurer_2.player_name)
-        expect(page).to have_content(adventurer.id)
-        expect(page).to have_content(adventurer.character_name)
-        expect(page).to have_content(adventurer.character_level)
-        expect(page).to have_content(adventurer.character_age)
-        expect(page).to have_content(adventurer.character_class)
-        expect(page).to have_content(adventurer.character_race)
-        expect(page).to have_content(adventurer.is_alive)
-        expect(page).to have_content(adventurer.created_at)
-        expect(page).to have_content(adventurer.updated_at)
-    end
+            visit '/player_characters'
+            # save_and_open_page
+            expect(page).to have_content(adventurer.player_name)
+            expect(page).to have_content(adventurer_2.player_name)
+            expect(page).to have_content(adventurer.id)
+            expect(page).to have_content(adventurer.character_name)
+            expect(page).to have_content(adventurer.character_level)
+            expect(page).to have_content(adventurer.character_age)
+            expect(page).to have_content(adventurer.character_class)
+            expect(page).to have_content(adventurer.character_race)
+            expect(page).to have_content(adventurer.is_alive)
+            expect(page).to have_content(adventurer.created_at)
+            expect(page).to have_content(adventurer.updated_at)
+        end
+    end 
 end
